0f84cdf362615e40d622ae1e9bf3be1445cbf36f
Lashawn Butler <lashawnbutler50@gmail.com>
2023-12-12 17:13:02 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet < 400){return 'This one is on me!'}
+  if(feet > 400 && feet < 2000){return 'That will be twenty bucks.'}
+  if(feet > 2500){return 'No can do.'}
+  if(feet > 2000){return 'I will gladly take your thirty bucks.'}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return city === 'NYC'? 'Ok, sounds good.': 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip === 'generous' ){return 'Thank you so much.'}
+  if(tip === 'not as generous'){return 'Thank you.'}
+  return 'Bye.'
 }
